01a6953ecbc1b05f34fe075e2633de93b602c912
Tim Culverhouse <tim@timculverhouse.com>
2024-10-29 12:09:21 -0500

    widget(Center): add doctest

--- src/Center.zig
+++ src/Center.zig
@@ -51,6 +51,68 @@ pub fn draw(self: *const Center, ctx: vtk.DrawContext) Allocator.Error!vtk.Surfa
     };
 }
 
+test Center {
+    const Text = @import("Text.zig");
+    // Will be height=1, width=3
+    const text: Text = .{ .text = "abc" };
+
+    const center: Center = .{ .child = text.widget() };
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const ucd = try vaxis.Unicode.init(arena.allocator());
+    vtk.DrawContext.init(&ucd, .unicode);
+
+    {
+        // Center expands to the max size. It must therefore have non-null max width and max height.
+        // These values are asserted in draw
+        const ctx: vtk.DrawContext = .{
+            .arena = arena.allocator(),
+            .min = .{},
+            .max = .{ .width = 10, .height = 10 },
+        };
+
+        const surface = try center.draw(ctx);
+        // Center does not produce any drawable cells
+        try std.testing.expectEqual(0, surface.buffer.len);
+        // Center has 1 child
+        try std.testing.expectEqual(1, surface.children.len);
+        // Center is the max size
+        try std.testing.expectEqual(surface.size, ctx.max.size());
+        const child = surface.children[0];
+        // The child is 1x3
+        try std.testing.expectEqual(3, child.surface.size.width);
+        try std.testing.expectEqual(1, child.surface.size.height);
+        // A centered 1x3 in 10x10 should be at origin 3, 4. The bias is toward the top left corner
+        try std.testing.expectEqual(4, child.origin.row);
+        try std.testing.expectEqual(3, child.origin.col);
+    }
+    {
+        // Center expands to the max size. It must therefore have non-null max width and max height.
+        // These values are asserted in draw
+        const ctx: vtk.DrawContext = .{
+            .arena = arena.allocator(),
+            .min = .{},
+            .max = .{ .width = 5, .height = 3 },
+        };
+
+        const surface = try center.draw(ctx);
+        // Center does not produce any drawable cells
+        try std.testing.expectEqual(0, surface.buffer.len);
+        // Center has 1 child
+        try std.testing.expectEqual(1, surface.children.len);
+        // Center is the max size
+        try std.testing.expectEqual(surface.size, ctx.max.size());
+        const child = surface.children[0];
+        // The child is 1x3
+        try std.testing.expectEqual(3, child.surface.size.width);
+        try std.testing.expectEqual(1, child.surface.size.height);
+        // A centered 1x3 in 3x5 should be at origin 1, 1. This is a perfectly centered child
+        try std.testing.expectEqual(1, child.origin.row);
+        try std.testing.expectEqual(1, child.origin.col);
+    }
+}
+
 test "refAllDecls" {
     std.testing.refAllDecls(@This());
 }
